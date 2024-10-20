c3e14e49644ff8dd095fb765eb8f9f1ac46346e1
nhan0204 <phamthiennhan0204@gmail.com>
2024-10-20 16:19:16 +0700

    auth-fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -30,7 +30,13 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <ClerkProvider>
+    <ClerkProvider
+      appearance={{
+        layout: {
+          unsafe_disableDevelopmentModeWarnings: true,
+        },
+      }}
+    >
       <html lang="en">
         <body
           className={`${geistSans.variable} ${geistMono.variable} antialiased`}
